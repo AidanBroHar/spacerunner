ebdd714f78d80980eb79be7df6774e360ab28676
AidanBroHar <aidanbro12@gmail.com>
2026-06-25 22:36:59 -0500

    cleaned up init file, instanciated pyproject file, created compile function

---
+++ spacerunner/compiler.py
@@ -0,0 +1,11 @@
+
+
+def compile(model_patht: str):
+    """Compile a ML model from a given path."""
+    #load, optimize, quantize, loadgen
+    return {
+        "model_bin": ...,
+        "c_runtume": ...,
+        "model_metadata": ...
+    }
+
